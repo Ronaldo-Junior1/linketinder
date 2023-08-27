cb4b2b9a9f5042d68eb71f29be9c4316f1a8952f
Ronaldo-Junior1 <ronaldosjr2005@gmail.com>
2023-08-27 18:01:02 -0300

    T002 - feat: Implementação da classe Empresa e do metodo listarEmpresas

--- src/Main.groovy
+++ src/Main.groovy
@@ -1,4 +1,5 @@
 import com.classes.Candidato
+import com.classes.Empresa
 
 static void main(String[] args) {
 
@@ -6,7 +7,12 @@ static void main(String[] args) {
           new Candidato("Ronaldo","email","São paulo","1838920","Dev Java",["Aaaa","aaaaa"],"11111",17),
           new Candidato("Vitor","email","São paulo","1838920","Dev Java",["Aaaa","aaaaa"],"11111",17)
   ]
+  def empresas = [
+          new Empresa("Arroz top","email","São paulo","1838920","Empresa mt top",["Aaaa", "aaaaa"],"11111","Brasil"),
+          new Empresa("Feijão top","email","São paulo","1838920","Empresa mt top",["Aaaa","aaaaa"],"11111","Argentina")
+  ]
 
-Candidato.listarCandidatos(candidatos)
+ Candidato.listarCandidatos(candidatos)
+ Empresa.listarEmpresa(empresas)
 }
 
---
+++ src/com/classes/Empresa.groovy
@@ -0,0 +1,33 @@
+package com.classes
+
+class Empresa extends Pessoa{
+    String cnpj
+    String pais
+
+    Empresa(String nome, String email, String estado, String cep, String descricao, ArrayList<String> competencias, String cnpj, String pais) {
+        super(nome, email, estado, cep, descricao, competencias)
+        this.cnpj = cnpj
+        this.pais = pais
+    }
+
+
+    @Override
+    String toString() {
+        return "Empresa{" +
+                "nome='"+ nome + '\'' +
+                ", email='"+ email + '\'' +
+                ", estado='"+ estado + '\'' +
+                ", cep='"+ cep + '\'' +
+                ", descrição='"+ descricao + '\'' +
+                ", competencias=" + competencias + '\''+
+                "cnpj='" + cnpj + '\'' +
+                ", pais='" + pais +
+                '}';
+    }
+
+    static listarEmpresa(List<Empresa> empresas) {
+        empresas.each { empresa ->
+            println(empresa)
+        }
+    }
+}
